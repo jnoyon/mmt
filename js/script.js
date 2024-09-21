c5d3dd244049a86c4c2b9b3213bd79ff87c79b5a
jnoyon <jihadur51@gmail.com>
2024-09-21 12:19:05 +0600

    section updated

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const loginButton = document.getElementById('login-button');
+
+
+// form submitt reload prevent
+
+loginButton.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('h');
+});
