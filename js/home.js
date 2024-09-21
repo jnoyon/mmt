ddf005a86d1335c2eea708ffdf7b72831ddb8012
jnoyon <jihadur51@gmail.com>
2024-09-21 13:21:11 +0600

    current balance section done

--- js/home.js
+++ js/home.js
@@ -4,10 +4,19 @@ let costAmount = document.getElementById('cost-amount');
 const costDesc = document.getElementById('cost-desc');
 
 
+
+
 addCostButton.addEventListener('click', function(e){
     e.preventDefault();
-    
-    console.log(costAmount.value);
-    
 
+    // convert string to numbers
+    const currentAmountN = parseFloat(currentAmount.innerText);
+    const costAmountN = parseFloat(costAmount.value);
+    const updatedBalance = currentAmountN-costAmountN;
+    
+    // update the money
+    currentAmount.innerText = updatedBalance;
+    costAmount.value = '';
+    costDesc.value = '';
+    
 });
