d26af857de109c9e5a1fc5f74fa270c0da625319
jnoyon <jihadur51@gmail.com>
2024-09-21 13:35:55 +0600

    section updated

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
 const addCostButton = document.getElementById('addcost-button');
 const currentAmount = document.getElementById('current-amount');
-let costAmount = document.getElementById('cost-amount');
+const costAmount = document.getElementById('cost-amount');
 const costDesc = document.getElementById('cost-desc');
-
+const todayCost = document.getElementById('today-cost');
 
 
 
@@ -12,10 +12,15 @@ addCostButton.addEventListener('click', function(e){
     // convert string to numbers
     const currentAmountN = parseFloat(currentAmount.innerText);
     const costAmountN = parseFloat(costAmount.value);
+    const todayCostN = parseFloat(todayCost.innerText);
+
     const updatedBalance = currentAmountN-costAmountN;
+    const updatedTodayCost = todayCostN+costAmountN;
     
     // update the money
     currentAmount.innerText = updatedBalance;
+    todayCost.innerText = updatedTodayCost;
+
     costAmount.value = '';
     costDesc.value = '';
     
