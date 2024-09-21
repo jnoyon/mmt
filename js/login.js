f8b1a06ad0a9e41cc16b4b70be29a3f59b69ec39
jnoyon <jihadur51@gmail.com>
2024-09-21 12:36:22 +0600

    login form updated

---
+++ js/login.js
@@ -0,0 +1,23 @@
+
+const loginButton = document.getElementById('login-button');
+const forgotPassword = document.getElementById('forgot-password');
+
+forgotPassword.addEventListener('click', function(){
+    alert('Phone Number is: 789 and Password: 1234');
+});
+
+loginButton.addEventListener('click', function(e){
+    e.preventDefault(); // form submitt reload prevent
+    
+    
+    const phoneNumber = document.getElementById('phone-number').value;
+    const password = document.getElementById('password').value;
+
+    if(phoneNumber === '789' && password === '1234'){
+        window.location.href = './home.html'
+    }
+    else {
+        alert('আপনার মোবাইল অথবা পাসওয়ার্ড ভুল হয়েছে');
+    }
+
+});
--- js/script.js
+++
@@ -1,9 +0,0 @@
-const loginButton = document.getElementById('login-button');
-
-
-// form submitt reload prevent
-
-loginButton.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log('h');
-});
